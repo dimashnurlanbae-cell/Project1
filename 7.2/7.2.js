55048181a799b151036fb096fdb66cc775f897c7
dimashnurlanbae-cell <dimashnurlanbae@gmail.com>
2026-03-06 11:35:10 +0500

    Add files via upload

---
+++ 7.2/7.2.js
@@ -0,0 +1,40 @@
+//Задание 1
+let num = 50
+console.log(num)
+num = 51
+console.log(num)
+const number = 100
+console.log(number)
+// number = 101; // Ошибка: присваивание значения константе
+//Задание 2
+//var — это способ объявлять переменные в JavaScript. Сейчас его считают устаревшим, потому что вместо него используют более удобные и безопасные let и const.
+//Задание 3
+let negative = -10;  
+console.log(typeof negative);
+let isTrue = true;
+console.log(typeof isTrue);
+let invalidOperation = 0 / 0; 
+console.log(invalidOperation);
+let undefinedValue;
+console.log(typeof underfinedValue);
+let user = null;
+console.log(user);
+//Задание 4
+let str = "123"
+let num2 = parseInt(str)
+console.log(str)
+let num1 = 123
+let str1 = toString(num1)
+console.log(num1)
+let undefinedv;
+let n = null;
+console.log(toString(undefinedv))
+console.log(parseInt(n))
+//Задание 5
+let integer = 60;
+console.log(toString(integer)) 
+//Задание 6
+let name = prompt("Введите имя")
+let age = prompt("Введите ваш возраст")
+console.log(name)
+console.log(age)
