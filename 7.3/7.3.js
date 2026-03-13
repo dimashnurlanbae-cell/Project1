0391b3e4f7c0d6fc9178da0c2b5e02550abe554d
dimashnurlanbae-cell <dimashnurlanbae@gmail.com>
2026-03-13 11:22:32 +0500

    Add files via upload

---
+++ 7.3/7.3.js
@@ -0,0 +1,42 @@
+//Задание 1
+let a = 10
+let b = 5
+let c = 2
+let sum = b + a + c;
+console.log(sum)
+let sumb = (a + c) - b;
+console.log(sumb)
+let sumc = (a * b) / c;
+console.log(sumc)
+let suma = a % b
+console.log(suma)
+//Задание 2
+x = 7
+let sqs = x++ + ++x
+let x2 = x-- - --x
+console.log(sqs)
+console.log(x2)
+//Задание 3
+let w = 4;
+let q = 5;
+let result = "The result is: " + (q + w);
+console.log(result);
+//Задание 4
+let ab = a>b && a>0 && b>0;
+let c10 = c<10 || c == 2;
+let ac = a/c != 5;
+console.log(ab)
+console.log(c10)
+console.log(ac)
+//Задание 5
+let n = 10 == "10";
+let n10 = 10 === "10";
+console.log(n)
+console.log(n10)
+//Задание 6
+x = 4
+let xtrue = (x%3 == 0 || x%2 == 0) && x%6 != 0;
+console.log(xtrue)
+let y = 16
+let yfalse = !(y>10 || y<5);
+console.log(yfalse)
