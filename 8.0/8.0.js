abf4df1fdc569ee583d371aa4a742a0dea6fb9cb
dimashnurlanbae-cell <dimashnurlanbae@gmail.com>
2026-03-23 11:37:45 +0500

    Add files via upload

---
+++ 8.0/8.0.js
@@ -0,0 +1,69 @@
+//Задание 1
+const greet = function() {
+   console.log("Привет мир!");
+};
+greet();
+//Задание 2
+let name1 = prompt("Введите ваше имя:")
+function sayHello(name1) {
+   console.log("Привет, " + name1 + "!");
+}
+sayHello(name1);
+//Задание 3
+function sum(a, b) {
+  return a + b;
+}
+let result = sum(5, 3);
+console.log(result);
+//Задание 4
+let num = Number(prompt("Введите четное или нечетное число:"));
+function isEven(num) {
+   return num % 2 == 0
+};
+console.log(isEven(num));
+//Задание 5
+let num1 = Number(prompt("Введите первое число:"));
+let num2 = Number(prompt("Введите второе число:"));
+function max(a, b) {
+   return a > b;
+}
+if ( max(num1,num2)) {
+   console.log("Первая больше");
+} else {
+   console.log("Первая меньше");
+}
+//Задание 6
+let name = prompt("Введите ваше имя:")
+let surname = prompt("Введите вашу фамилию:")
+function getInitials(name,surname) {
+   let namename = name[0]
+   let surnamesurname = surname[0]
+   return namename + "." + surnamesurname + ".";
+};
+console.log(getInitials(name,surname));
+//Задание 7 
+let cv = prompt("Введите любое число:")
+function square(cv) {
+   return cv * cv
+}
+console.log(square(cv));
+
+let cub = prompt("Введите любое число:")
+function cube(cub) {
+   return cub * square(cub)
+}
+console.log(cube(cub));
+//Задание 8 
+const sum2 = (a, b) => {
+  return a + b;
+}
+let result1 = sum2(5, 3);
+console.log(result1);
+//Доп. задание
+function getFactorial(factorial) {
+   if (factorial <= 1) {
+      return 1
+   }
+   return factorial * getFactorial(factorial - 1)
+}
+console.log(getFactorial(7))
